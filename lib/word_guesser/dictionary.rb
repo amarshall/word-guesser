f148618b67b0458247f33375baa84882a6e001c0
Andrew Marshall <andrew@johnandrewmarshall.com>
2012-02-03 23:10:25 -0500

    Use absolute path for loading dictionary

--- lib/word_guesser/dictionary.rb
+++ lib/word_guesser/dictionary.rb
@@ -1,6 +1,6 @@
 module WordGuesser
   class Dictionary
-    DICTIONARY_FILENAME = "lib/enable1.txt"
+    DICTIONARY_FILENAME = File.join(File.dirname(__FILE__), "enable1.txt")
 
     def self.words
       unless @dictionary
